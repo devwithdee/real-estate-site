5a3da25bf0306913396eb23431b09cd3aeb103da
djbohl <96339436+djbohl@users.noreply.github.com>
2022-12-23 17:08:21 -0500

    /

--- website/pages/[slug].js
+++ website/pages/[slug].js
@@ -57,8 +57,8 @@ export async function getStaticProps() {
   
     return {
      paths: [
-         '/[...slug]',
-         { params: { slug: '/[...slug]'}},
+         '/[slug]',
+         { params: { slug: '/[slug]'}},
      ],
      fallback: false,
     }
