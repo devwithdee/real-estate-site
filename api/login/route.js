7f37f711a9b9815f21daf473c54aaab52ffa630b
djbohl <donna.s.bohl@gmail.com>
2024-02-05 23:38:01 -0500

    Updated login api

--- api/login/route.js
+++ api/login/route.js
@@ -1,10 +1,7 @@
 import { db } from '@vercel/postgres';
 import { hash, compare } from 'bcrypt';
-import Cors from 'cors';
 
 export default async function login(req, res) {
-
-
   try {
     const { email, password } = req.body;
     console.log('User Input Password:', password);
