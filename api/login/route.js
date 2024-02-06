53b233ebd4b357e7c996de9bfb9147ccf56e4c01
djbohl <donna.s.bohl@gmail.com>
2024-02-05 22:44:36 -0500

    Renamed api

--- api/login/route.js
+++ api/login/route.js
@@ -11,21 +11,20 @@ export default async function login(req, res) {
     try {
       client = await db.connect();
 
-      const hashedPassword = await hash(password, 10);
-
       const result = await client.query(
         'SELECT email, password FROM users WHERE email = $1',
         [email]
       );
-
+      
       if (result.rows.length === 0) {
         return res.status(401).json({ error: 'User not found' });
       }
-
+      
       const dbPassword = result.rows[0].password;
+    
       const passwordMatch = await compare(password, dbPassword);
       console.log('Database Password:', dbPassword);
-
+      
       if (!passwordMatch) {
         console.error('Invalid password');
         return res.status(401).json({ error: 'Invalid password' });
