b76b76091d19602aa3d63196041e007a5ea770e2
djbohl <donna.s.bohl@gmail.com>
2024-02-23 04:21:06 -0500

    added account data and application cards

--- src/app/(portal)/portal/account/page.js
+++ src/app/(portal)/portal/account/page.js
@@ -1,14 +1,77 @@
 "use client"
 import { useAccount } from "../../../../../context/account";
+import React, { useState } from "react";
+import styles from '../../../styles/account.module.css';
 
 export default function Account() {
-    const { userEmail, userFirstName, userLastName} = useAccount();
+    const { userEmail, userFirstName, userLastName } = useAccount();
+
+    const userPhone = null;
+
+    const cards = [
+        { location: "loc", availableOn: "date",  description: "This is card 1", bedrooms: 1, applicationStatus: "Pending" },
+        { location: "loc", availableOn: "date",  description: "This is card 2",bedrooms: 1,  applicationStatus: "Pending" },
+        { location: "loc", availableOn: "date",  description: "This is card 3",bedrooms: 1,  applicationStatus: "Pending" },
+    ];
+
     return (
         <>
-        <h1>User Account</h1>
-        <p>Email: {userEmail}</p>
-        <p>First Name: {userFirstName}</p>
-        <p>Last Name: {userLastName}</p>
+            <div className={styles.layout}>
+                <form className={styles.form}>
+                <h1 className={styles.h1}>Account Details</h1>
+
+                    <div className="form-group">
+                        <label htmlFor="firstName">First Name</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="firstName"
+                            value={userFirstName}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="lastName">Last Name</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="lastName"
+                            value={userLastName}
+
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="email"
+                            value={userEmail}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="phone">Phone (optional)</label>
+                        <input
+                            type="tel"
+                            className="form-control"
+                            id="phone"
+                            value={userPhone}
+                        />
+                    </div>
+                    <button type="submit" className={`btn btn-primary ${styles.button}`}>Submit</button>
+                </form>
+
+                <div>
+                    <h2 className={styles.h1}>View Applications</h2>
+                    {cards.map((card, index) => (
+                        <div key={index} className={styles.card}>
+                            <h2 className={styles.h1}>Location: {card.location}</h2>
+                            <p>Available On: {card.availableOn}</p>
+                            <p># of Bedrooms: {card.bedrooms}</p>
+                            <p>Application Status: {card.applicationStatus}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </>
     );
 }
